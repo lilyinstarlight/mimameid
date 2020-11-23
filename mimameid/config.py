0dc684a6eccb9101e0be5cde0a08b194bccb9e6f
Lily Foster <lily@lily.flowers>
2020-11-23 16:48:56 -0500

    set log level to info

--- mimameid/config.py
+++ mimameid/config.py
@@ -61,6 +61,8 @@ def _apply():
     else:
         _logging.getLogger('mimameid').addHandler(_logging.StreamHandler(_sys.stdout))
 
+    _logging.getLogger('mimameid').setLevel(_logging.INFO)
+
     if http_log:
         http_log_handler = _logging.FileHandler(http_log)
         http_log_handler.setFormatter(_web.HTTPLogFormatter())
